0d0fcaed1d8e981843b6629bc39c59d160e37b9e
Mat Baker <mu.beta.06@gmail.com>
2012-10-05 15:21:41 -0600

    Added initial crack at facebook style sliding panel widget.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 android
 =======
 
-SlidingPanel
+Repository containing my android shenanigans.
---
+++ SlidingPanel/src/com/mubeta/slidingpanel/MainActivity.java
@@ -0,0 +1,35 @@
+package com.mubeta.slidingpanel;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      if (false) setContentView(R.layout.activity_main);
+      else {
+        ViewGroup decorView = (ViewGroup) getWindow().getDecorView();
+        View oldScreen = decorView.getChildAt(0);
+        decorView.removeViewAt(0);
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        SlidingPanel slidingPanel = (SlidingPanel) inflater.inflate(R.layout.screen_slider, null);
+        ((ViewGroup) slidingPanel.findViewById(R.id.anterior)).addView(oldScreen);
+        decorView.addView(slidingPanel, 0);
+        setContentView(R.layout.anterior);
+      }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+      getMenuInflater().inflate(R.menu.activity_main, menu);
+      return true;
+    }
+
+}
